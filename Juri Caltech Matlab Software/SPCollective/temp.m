d4eef5176e5375c1f48928902bced10e9aa19bf0
Alex Barreiro <alexb@adaptly.com>
2015-02-14 12:30:54 -0800

    introduced all-in-one program (AIO)

--- Juri Caltech Matlab Software/SPCollective/temp.m
+++
@@ -1,9 +0,0 @@
-% Read the data
-filename = 'G25070.dat';
-fid=fopen(filename,'r');
-signal=fscanf(fid,'%f');
-fclose(fid);
-
-Fs = 2500;
-subplot(3,1,1); plot(signal)
-subplot(3,1,2); plot(abs(fftshift(fft(signal))));
